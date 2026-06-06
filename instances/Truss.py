9ff08ecc9450d9a4835d133664f073a5e126123c
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-06-06 12:13:44 -0400

    remove Climbable

--- instances/Truss.py
+++ instances/Truss.py
@@ -1,5 +1,10 @@
-from .Climbable import Climbable
-class Truss(Climbable):
+from .Part import Part
+class Truss(Part):
     ClassName = "Truss"
+    Properties = [
+        ["ClimbSpeed", "float"],
+    ]
     def __init__(self):
         super().__init__()
+        self.addProperties(Truss.Properties)
+        self.ClimbSpeed = 1
