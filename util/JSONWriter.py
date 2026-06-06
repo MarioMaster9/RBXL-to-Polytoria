c5c0c0855c892c07813d024862e46cbc5b2eab7c
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-06-06 13:36:04 -0400

    change indent level

--- util/JSONWriter.py
+++ util/JSONWriter.py
@@ -5,11 +5,10 @@ class JSONWriter:
     def __init__(self, fileName):
         self.fileName = fileName
     def write(self, data):
-        jsonStr = json.dumps(data, indent=4)
-        jsonStrNoFormatting = json.dumps(data)
-        zstdData = zstd.compress(jsonStrNoFormatting.encode('utf-8'))
+        jsonStr = json.dumps(data, indent=2)
+        zstdData = zstd.compress(jsonStr.encode('utf-8'))
         with open(self.fileName, 'w') as f:
-            f.write(jsonStrNoFormatting)
+            f.write(jsonStr)
         with open(f'{self.fileName}.zst', 'wb+') as f:
             f.write(zstdData)
     def close(self):
