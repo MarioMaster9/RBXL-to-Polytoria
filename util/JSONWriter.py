b4f84a2f8e08ad0004b19d3a45e2b1dea7626a9a
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-06-06 13:34:18 -0400

    change the compression to actually use zstd

--- util/JSONWriter.py
+++ util/JSONWriter.py
@@ -1,15 +1,16 @@
 import json
-import zlib
+import zstd
 
 class JSONWriter:
     def __init__(self, fileName):
         self.fileName = fileName
     def write(self, data):
         jsonStr = json.dumps(data, indent=4)
-        zlibData = zlib.compress(jsonStr.encode('utf-8'))
+        jsonStrNoFormatting = json.dumps(data)
+        zstdData = zstd.compress(jsonStrNoFormatting.encode('utf-8'))
         with open(self.fileName, 'w') as f:
-            f.write(jsonStr)
+            f.write(jsonStrNoFormatting)
         with open(f'{self.fileName}.zst', 'wb+') as f:
-            f.write(zlibData)
+            f.write(zstdData)
     def close(self):
         pass
