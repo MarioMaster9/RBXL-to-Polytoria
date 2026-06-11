dc9fdd3d97d4473b4228b14522437a340a5b4092
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-06-11 10:53:13 -0400

    sunlight

--- instances/SunLight.py
+++ instances/SunLight.py
@@ -1,15 +1,9 @@
-from .DynamicInstance import DynamicInstance
-from rbxl.data_types import Color4, Vector3
-class SunLight(DynamicInstance):
+from .Dynamic import Dynamic
+from rbxl.data_types import Vector3
+class SunLight(Light):
     ClassName = "SunLight"
-    Properties = [
-        ["Brightness", "float"],
-        ["Color", "color"],
-        ["Shadows", "boolean"],
-    ]
     def __init__(self):
         super().__init__()
-        self.addProperties(SunLight.Properties)
         self.Position = Vector3(0, 15, 0)
         self.Size = Vector3.ONE
         self.Shadows = True
