e6533ae8216cec4b3c529e67c25c09e7c7407559
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-06-15 13:30:30 -0400

    instance stuff

--- instances/__init__.py
+++ instances/__init__.py
@@ -1,38 +1,58 @@
+from .resources.AudioAsset                import AudioAsset
+from .resources.BaseAsset                 import BaseAsset
+from .resources.BuiltInFontAsset          import BuiltInFontAsset
+from .resources.FontAsset                 import FontAsset
+from .resources.ImageAsset                import ImageAsset
+from .resources.MeshAsset                 import MeshAsset
+from .resources.PTAudioAsset              import PTAudioAsset
+from .resources.PTImageAsset              import PTImageAsset
+from .resources.PTMeshAsset               import PTMeshAsset
+from .resources.ResourceAsset             import ResourceAsset
 from .resources.ResourceFactory           import ResourceFactory
 
 from .services.AchievementsService        import AchievementsService
-from .Animator                            import Animator
-from .resources.AudioAsset                import AudioAsset
-from .resources.BaseAsset                 import BaseAsset
-from .BodyPosition                        import BodyPosition
-from .BoolValue                           import BoolValue
-from .resources.BuiltInFontAsset          import BuiltInFontAsset
-from .Camera                              import Camera
 from .services.CaptureService             import CaptureService
-from .ColorValue                          import ColorValue
 from .services.CoreUIService              import CoreUIService
+from .services.ScriptService              import ScriptService
+
+
+#Accessory
+from .Animator                            import Animator
+#BindableEvent
+from .BodyPosition                        import BodyPosition
+#BodyRotation
+from .BoolValue                           import BoolValue
+from .Camera                              import Camera
+from .CharacterModel                      import CharacterModel
+from .ClientScript                        import ClientScript
+from .Clothing                            import Clothing
+#ColorAdjustModifier
+from .ColorValue                          import ColorValue
 from .Decal                               import Decal
+from .Dynamic                             import Dynamic
 from .Entity                              import Entity
 from .Environment                         import Environment
 from .Explosion                           import Explosion
 from .Folder                              import Folder
-from .resources.FontAsset                 import FontAsset
+#Grabbable
+#GradientSky
 from .GUI                                 import GUI
 from .GUI3D                               import GUI3D
 from .Hidden                              import Hidden
 from .HiddenBase                          import HiddenBase
 from .Image3D                             import Image3D
-from .resources.ImageAsset                import ImageAsset
 from .ImageSky                            import ImageSky
 from .Instance                            import Instance
 from .InstanceValue                       import InstanceValue
+#InteractionPrompt
 from .IntValue                            import IntValue
 from .Inventory                           import Inventory
 from .Light                               import Light
 from .Lighting                            import Lighting
-from .ClientScript                        import ClientScript
+#LightingModifier
+#Marker3D
 from .Mesh                                import Mesh
-from .resources.MeshAsset                 import MeshAsset
+#MissingInstance
 from .Model                               import Model
 from .ModuleScript                        import ModuleScript
 from .NetworkedObject                     import NetworkedObject
@@ -40,34 +60,59 @@ from .NetworkEvent                        import NetworkEvent
 from .NPC                                 import NPC
 from .NumberValue                         import NumberValue
 from .Part                                import Part
+from .Particles                           import Particles
+from .Physical                            import Physical
+#Player
 from .PlayerDefaults                      import PlayerDefaults
 from .PlayerGUI                           import PlayerGUI
 from .Players                             import Players
 from .PointLight                          import PointLight
-from .resources.PTAudioAsset              import PTAudioAsset
-from .resources.PTImageAsset              import PTImageAsset
-from .resources.PTMeshAsset               import PTMeshAsset
-from .resources.ResourceAsset             import ResourceAsset
+from .PolytorianModel                     import PolytorianModel
+#ProceduralSky
+#QuaternionValue
+#RangeValue
+from .RigidBody                           import RigidBody
 from .Script                              import Script
-from .services.ScriptService              import ScriptService
 from .Seat                                import Seat
 from .ServerHidden                        import ServerHidden
 from .ServerScript                        import ServerScript
+#Sky
 from .Sound                               import Sound
 from .SpotLight                           import SpotLight
+#Stat
 from .Stats                               import Stats
 from .StringValue                         import StringValue
 from .SunLight                            import SunLight
 from .Team                                import Team
 from .Teams                               import Teams
+#Temporary
+from .Text3D                              import Text3D
 from .Tool                                import Tool
 from .Truss                               import Truss
+#UIAspectRatioRestraint
 from .UIButton                            import UIButton
+#UIContainer
+#UICorner
+from .UIField                             import UIField
+#UIFlowLayout
+#UIGridLayout
+#UIHFlow
+#UIHLayout
+#UIHVLayout
 from .UIImage                             import UIImage
 from .UILabel                             import UILabel
+#UIScrollView
+#UIShadow
+#UIStroke
 from .UITextInput                         import UITextInput
+#UIVFlow
 from .UIView                              import UIView
+#UIViewport
+#UIVLayout
+from .ValueBase                           import ValueBase
+#Vector2Value
 from .Vector3Value                        import Vector3Value
+#VoiceBox
 from .Weld                                import Weld
 from .World                               import World
 from .Game                                import Game
